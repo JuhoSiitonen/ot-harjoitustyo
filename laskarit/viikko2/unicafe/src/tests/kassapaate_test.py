a015339fee2409d65505210deccb23aadab3c006
Juho Siitonen <juho_tuomas@msn.com>
2023-03-23 19:36:24 +0200

    made testsss

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,9 +1,11 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
-        self.kassapaate = Kassapaate(100000)
+        self.kassapaate = Kassapaate()
+        self.kortti = Maksukortti(1000)
 
     def test_luodun_kassapaatteen_saldo_oikea(self):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
@@ -11,4 +13,120 @@ class TestKassapaate(unittest.TestCase):
     def test_alussa_lounaiden_maara_nolla(self):
         self.assertEqual((self.kassapaate.edulliset + self.kassapaate.maukkaat), 0)
 
-    def test_
+    def test_syo_maukkaasti_kateisella_riittava_maksu_vaihtoraha(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(500), 100)
+
+    def test_syo_maukkaasti_kateisella_riittava_maksu_kassa(self):
+        self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
+
+    def test_syo_maukkaasti_kateisella_riittava_maksu_lounas_maara(self):
+        self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_syo_maukkaasti_kateisella_riittamaton_maksu_vaihtoraha(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(100), 100)
+
+    def test_syo_maukkaasti_kateisella_riittamaton_maksu_kassa(self):
+        self.kassapaate.syo_maukkaasti_kateisella(100)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_syo_maukkaasti_kateisella_riittamaton_maksu_lounas_maara(self):
+        self.kassapaate.syo_maukkaasti_kateisella(100)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_syo_edullisesti_kateisella_riittava_maksu_vaihtoraha(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(500), 260)
+
+    def test_syo_edullisesti_kateisella_riittava_maksu_kassa(self):
+        self.kassapaate.syo_edullisesti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
+
+    def test_syo_edullisesti_kateisella_riittava_maksu_lounas_maara(self):
+        self.kassapaate.syo_edullisesti_kateisella(500)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_syo_edullisesti_kateisella_riittamaton_maksu_vaihtoraha(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(100), 100)
+
+    def test_syo_edullisesti_kateisella_riittamaton_maksu_kassa(self):
+        self.kassapaate.syo_edullisesti_kateisella(100)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_syo_edullisesti_kateisella_riittamaton_maksu_lounas_maara(self):
+        self.kassapaate.syo_edullisesti_kateisella(100)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_syo_edullisesti_kortilla_riittava_maksu_true(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.kortti), True)
+
+    def test_syo_edullisesti_kortilla_riittava_maksu_veloitus(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.60 euroa")
+
+    def test_syo_edullisesti_kortilla_riittava_maksu_lounaat(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_syo_edullisesti_kortilla_riittamaton_maksu_veloitus(self):
+        kortti = Maksukortti(100)
+        self.kassapaate.syo_edullisesti_kortilla(kortti)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 1.00 euroa")
+
+    def test_syo_edullisesti_kortilla_riittamaton_maksu_false(self):
+        kortti = Maksukortti(100)
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(kortti), False)
+
+    def test_syo_edullisesti_kortilla_riittamaton_maksu_lounaat(self):
+        kortti = Maksukortti(100)
+        self.kassapaate.syo_edullisesti_kortilla(kortti)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_syo_maukkaasti_kortilla_riittava_maksu_true(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.kortti), True)
+
+    def test_syo_maukkaasti_kortilla_riittava_maksu_veloitus(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6.00 euroa")
+
+    def test_syo_maukkaasti_kortilla_riittava_maksu_lounaat(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_syo_maukkaasti_kortilla_riittamaton_maksu_veloitus(self):
+        kortti = Maksukortti(100)
+        self.kassapaate.syo_maukkaasti_kortilla(kortti)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 1.00 euroa")
+
+    def test_syo_maukkaasti_kortilla_riittamaton_maksu_false(self):
+        kortti = Maksukortti(100)
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(kortti), False)
+
+    def test_syo_maukkaasti_kortilla_riittamaton_maksu_lounaat(self):
+        kortti = Maksukortti(100)
+        self.kassapaate.syo_maukkaasti_kortilla(kortti)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_syo_maukkaasti_kortilla_ei_muuta_kassaa(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_syo_edullisesti_kortilla_ei_muuta_kassaa(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_lataa_rahaa_kortille_saldo_kasvaa(self):
+        self.kassapaate.lataa_rahaa_kortille(self.kortti, 100)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_lataa_rahaa_kortille_kassa_kasvaa(self):
+        self.kassapaate.lataa_rahaa_kortille(self.kortti, 100)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100100)
+
+    def test_lataa_rahaa_kortille_negatiivinen_summa_kortin_saldo(self):
+        self.kassapaate.lataa_rahaa_kortille(self.kortti, -100)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataa_rahaa_kortille_negatiivinen_kassa_ei_kasva(self):
+        self.kassapaate.lataa_rahaa_kortille(self.kortti, -100)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
