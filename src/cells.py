17ec84b05a6e4f31090f7e159aa655730cb621e7
Juho Siitonen <juho_tuomas@msn.com>
2023-04-13 20:37:10 +0300

    Adjusted Cell.py

--- src/cells.py
+++ src/cells.py
@@ -1,11 +1,12 @@
 import pygame
 
+
 class Cell(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
         self.image.fill("white")
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
 
     def camera(self, x_dx):
         self.rect.x += x_dx
