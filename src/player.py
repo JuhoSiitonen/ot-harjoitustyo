2a7daccdff5894112594f188276cb210b5709184
Juho Siitonen <juho_tuomas@msn.com>
2023-04-13 20:38:37 +0300

    Adjusted player.py

--- src/player.py
+++ src/player.py
@@ -1,12 +1,12 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.image = pygame.Surface((32, 64))
         self.image.fill("green")
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
 
     def movement(self, dx=0, dy=0):
         self.rect.move_ip(dx, dy)
-
