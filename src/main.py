a608a4b230d45ba57b5bf994a34ba429c6546cc9
Juho Siitonen <juho_tuomas@msn.com>
2023-04-16 20:03:36 +0300

    Changed main.py to have injected classes for all modules

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
-import pygame, sys
+import pygame
+import sys
 from level import Level
+from game import Game
+from renderer import Renderer
+from event_handling import Event_handling
+from clock import Clock
+
 
 level_map = ['00000000000000000000',
              '00000000000000000000',
@@ -13,24 +19,22 @@ level_map = ['00000000000000000000',
 
 CELL_SIZE = 64
 
+
 def main():
     display_height = CELL_SIZE * len(level_map)
     display_width = 1200
     display = pygame.display.set_mode((display_width, display_height))
+    
+    clock = Clock()
+    level = Level(level_map)
+    renderer = Renderer(display, level)
+    event_handling = Event_handling()
+    game = Game(level, clock, event_handling, renderer)
+
     pygame.display.set_caption("Jumpman")
     pygame.init()
-    clock = pygame.time.Clock()
-    level = Level(level_map)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        display.fill("black")
-        level.all_sprites.draw(display)
-        pygame.display.update()
-        clock.tick(60)
+    game.start()
 
 if __name__ == "__main__":
     main()
