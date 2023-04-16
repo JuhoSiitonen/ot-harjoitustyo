ac2c888c8fa932d0320ababf120f8782d729cceb
Juho Siitonen <juho_tuomas@msn.com>
2023-04-16 21:35:03 +0300

    Created method input for key pressed checking and also direction vector for smooth movement

--- src/player.py
+++ src/player.py
@@ -7,6 +7,19 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill("green")
         self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 7
+
+    def input(self):
+        inputs = pygame.key.get_pressed()
+        if inputs[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif inputs[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+        change = self.direction.x * self.speed
+        self.movement(dx=change)
 
     def movement(self, dx=0, dy=0):
         self.rect.move_ip(dx, dy)
