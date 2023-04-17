f31c5542259fbfdcecc5515b508984b7212c6a0f
Juho Siitonen <juho_tuomas@msn.com>
2023-04-17 15:15:55 +0300

    Pylint suggested changes

--- src/level.py
+++ src/level.py
@@ -2,7 +2,6 @@ import pygame
 from cells import Cell
 from player import Player
 
-
 class Level:
     def __init__(self, level_map):
         self.setup(level_map)
@@ -15,8 +14,8 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         for row_index, row in enumerate(level_map):
             for col_index, col in enumerate(row):
-                x = col_index * 64
-                y = row_index * 64
+                x = col_index * 64 # pylint: disable=invalid-name
+                y = row_index * 64 # pylint: disable=invalid-name
                 if col == "x":
                     self.cell = Cell((x, y), 64, "white")
                     self.cells.add(self.cell)
@@ -33,7 +32,7 @@ class Level:
         )
 
     def camera(self):
-        x = self.player.get_player_x()
+        x = self.player.get_player_x() # pylint: disable=invalid-name
         direction = self.player.get_direction()
         if x < 200 and direction < 0:
             self.camera_shift = 7
@@ -41,10 +40,10 @@ class Level:
         elif x > 1000 and direction > 0:
             self.camera_shift = -7
             self.player.speed = 0
-        else: 
+        else:
             self.camera_shift = 0
             self.player.speed = 7
-    
+
     def horizontal_collision(self):
         player = self.player_cell.sprite
 
@@ -63,6 +62,6 @@ class Level:
                 if player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
-                elif player.direction.y > 0: 
+                elif player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
