182f6c57ec3d8b9d902732d9fa9b399c7a0b93d4
Juho Siitonen <juho_tuomas@msn.com>
2023-04-20 23:38:10 +0300

    Added update for the level goal sprite

--- src/renderer.py
+++ src/renderer.py
@@ -8,6 +8,7 @@ class Renderer:
     def render(self):
         self.display.fill("black")
         self.level.cells.update(self.level.camera_shift)
+        self.level.goal.update(self.level.camera_shift)
         self.level.all_sprites.draw(self.display)
         pygame.display.update()
         self.level.camera()
