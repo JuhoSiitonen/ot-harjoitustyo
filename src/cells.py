42f4b018fb902d4aa3f665501bf34ed3d5041793
Juho Siitonen <juho_tuomas@msn.com>
2023-04-21 02:24:57 +0300

    Changed initializing cells to have a lenght and height variable

--- src/cells.py
+++ src/cells.py
@@ -2,9 +2,9 @@ import pygame
 
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, pos, size, color):
+    def __init__(self, pos, size_y, size_x, color):
         super().__init__()
-        self.image = pygame.Surface((size, size))
+        self.image = pygame.Surface((size_y, size_x))
         self.color = color
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft=pos)
