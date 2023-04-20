9602001f298712d35191d3bf1c8fc0711400d9d3
Juho Siitonen <juho_tuomas@msn.com>
2023-04-21 02:26:12 +0300

    Added rendering for enemies and their invisible blocking cells

--- src/renderer.py
+++ src/renderer.py
@@ -9,6 +9,8 @@ class Renderer:
         self.display.fill("black")
         self.level.cells.update(self.level.camera_shift)
         self.level.goal.update(self.level.camera_shift)
+        self.level.enemies.update(self.level.camera_shift)
+        self.level.blocker.update(self.level.camera_shift)
         self.level.all_sprites.draw(self.display)
         pygame.display.update()
         self.level.camera()
