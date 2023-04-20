e26585f6ffd5ef763287423eb981e8b86a0f8f69
Juho Siitonen <juho_tuomas@msn.com>
2023-04-20 23:31:20 +0300

    Created settings file to store level files

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+level_map_1 = ['00000000000000000000',
+             '00000000000000000000',
+             '00000000000000000000',
+             'xxx00000000000000000',
+             'xxx000000P0G00000xxx',
+             '0000000xxxxx00000xxx',
+             '0000000xxxxx00000xxx',
+             'xxxxx000000000000000',
+             'xxxxxxxx000000xxxxxx']
+
+CELL_SIZE = 64
+display_height = CELL_SIZE * len(level_map_1)
+display_width = 1200
