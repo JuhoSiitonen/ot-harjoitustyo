ea6bfa5a911d454d7f1a99ccbfa82119e90def9e
Juho Siitonen <juho_tuomas@msn.com>
2023-04-27 01:57:11 +0300

    Added time attack mode basics, renderer renders a timer when playing

--- src/support/renderer.py
+++ src/support/renderer.py
@@ -1,14 +1,22 @@
 import pygame
+import time
 
 class Renderer:
-    def __init__(self, display, level):
+    def __init__(self, display, level, time_attack):
         self.display = display
         self.level = level
+        self.time_attack = time_attack
+        self.start = time.time()
 
     def counter_text(self, count, color, placement):
         fontt = pygame.font.SysFont("Arial", 40)
         counter = fontt.render(f"{count}", True, color)
         self.display.blit(counter, placement)
+
+    def time_counter(self):
+        if self.time_attack:
+            counter = round((time.time() - self.start), 2)
+            self.counter_text(counter, "white", (100, 25))
 
     def render(self):
         self.display.fill("black")
@@ -16,5 +24,6 @@ class Renderer:
         self.level.all_sprites.draw(self.display)
         self.counter_text(self.level.player.coins, "yellow", (1100, 25))
         self.counter_text(self.level.player.artifacts, "red", (1100, 70))
+        self.time_counter()
         pygame.display.update()
         self.level.camera()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -12,22 +12,32 @@ from settings import level_map_1, level_map_2, DISPLAY_HEIGHT, DISPLAY_WIDTH
 class UI:
     def __init__(self):
         self.create_window()
-        self.max_level = 1
+        self.time_attack = False
 
     def create_window(self):
         sg.set_options(font = "Franklin 20")
         self.layout = [
             [sg.Text("Select level")],
             [sg.Button("Level 1"), sg.Button("Level 2")],
-            [sg.Button("Exit")]
+            [sg.Button("Time Attack"), sg.Button("Exit")]
             ]
         self.window = sg.Window("Jumpman", self.layout)
+
+    def check_time_attack(self):
+        if not self.time_attack:
+            self.time_attack = True
+            self.window["Time Attack"].update(button_color = ("blue"))
+        else:
+            self.time_attack = False
+            self.window["Time Attack"].update(button_color = None)
 
     def run(self):
         while True:
             event, values = self.window.read() # pylint: disable=unused-variable
             if event in (sg.WIN_CLOSED, "Exit"):
                 sys.exit()
+            if event == "Time Attack":
+                self.check_time_attack()
             if event == "Level 1":
                 pygame_thread = threading.Thread(target=self.run_game(level_map_1))
                 pygame_thread.start()
@@ -41,7 +51,7 @@ class UI:
 
         clock = Clock()
         level = Level(level_map)
-        renderer = Renderer(display, level)
+        renderer = Renderer(display, level, self.time_attack)
         event_handling = EventHandling()
         game = Game(level, clock, event_handling, renderer)
 
