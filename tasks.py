e016fa1c27052bded15d19d1284b86dc13d17d48
Juho Siitonen <juho_tuomas@msn.com>
2023-04-30 15:27:25 +0300

    Deleted build task

--- tasks.py
+++ tasks.py
@@ -5,10 +5,6 @@ def start(ctx):
     ctx.run("python3 src/main.py", pty=True)
 
 @task
-def build(ctx):
-    ctx.run("python3 src/build.py", pty=True)
-
-@task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
