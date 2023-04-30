ea3b5d68ea64f711cd2fd025fc9f27d2a30a492b
Juho Siitonen <juho_tuomas@msn.com>
2023-04-30 14:45:10 +0300

    Created new tests

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -4,7 +4,7 @@ from logic.level import Level
 from logic.game import Game
 
 class StubClock:
-    def tick(self, fps):
+    def tick(self):
         pass
 
 class StubKey_press:
@@ -12,18 +12,31 @@ class StubKey_press:
         self.key = key
     
     def get(self):
-        pressed = {}
+        pressed = {pygame.K_RIGHT : False, pygame.K_LEFT : False, 
+                   pygame.K_SPACE : False}
         pressed[self.key] = True
         return pressed
 
+class StubEvent:
+    def __init__(self, event_type, key):
+        self.type = event_type
+        self.key = key
+
 class StubEvent_handling:
-    def __init__(self, events):
+    def __init__(self, events, key_pressed):
         self.events = events
+        self.pressed = key_pressed
 
     def get(self):
         return self.events
     
+    def get_pressed(self):
+        return self.pressed
+    
 class StubRenderer:
+    def __init__(self):
+        self.timeout = False
+
     def render(self):
         pass
 
@@ -31,7 +44,7 @@ level_map = ['00000000000000000000',
              '00000000000000000000',
              '00000000000000000000',
              'xxx000000x0000000000',
-             'xxx000000PG000000xxx',
+             'xxx0000BEPGB00000xxx',
              '0000000xxxxx00000xxx',
              '0000000xxxxx00000xxx',
              'xxxxx000000000000000',
@@ -41,16 +54,54 @@ class TestGameLoop(unittest.TestCase):
     def setUp(self):
         self.level = Level(level_map)
 
-    """
-    def test_game(self):
-        pressed = StubKey_press("pygame.K_RIGHT").get()
+    
+    def test_game_can_complete_level(self):
+        pressed = StubKey_press(pygame.K_RIGHT).get()
+        events = [
+            StubEvent(pygame.KEYDOWN, pygame.K_LEFT),
+        ]
         game = Game(
             self.level,
             StubClock(),
-            StubEvent_handling(pressed),
+            StubEvent_handling(events, pressed),
             StubRenderer()
         )
         game.start()
-    """
+        self.assertEqual(self.level.level_completion(), True)
+
+    def test_game_window_closed(self):
+        pressed = StubKey_press(pygame.K_LEFT).get()
+        events = [
+            StubEvent(pygame.QUIT, None),
+        ]
+        game = Game(
+            self.level,
+            StubClock(),
+            StubEvent_handling(events, pressed),
+            StubRenderer()
+        )
+        game.start()
+        self.assertEqual(game.handle_events(), False)
+
+    def test_player_demise_works(self):
+        pressed = StubKey_press(pygame.K_LEFT).get()
+        events = [
+            StubEvent(pygame.QUIT, None),
+        ]
+        game = Game(
+            self.level,
+            StubClock(),
+            StubEvent_handling(events, pressed),
+            StubRenderer()
+        )   
+        game.start()
+        player = self.level.player_cell.sprite
+        x_value = player.rect.x
+        for enemy in self.level.enemies:
+            enemy.rect.x = x_value 
+        self.assertEqual(self.level.player_demise(), True)
+        pygame.quit()
+
+    
 
 
