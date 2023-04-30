8ec5343d927f4ba394bf99b68d32d34bd993e2f9
Juho Siitonen <juho_tuomas@msn.com>
2023-04-30 15:24:31 +0300

    Added invoke task to build program

--- tasks.py
+++ tasks.py
@@ -5,6 +5,10 @@ def start(ctx):
     ctx.run("python3 src/main.py", pty=True)
 
 @task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+@task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
