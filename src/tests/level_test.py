d352c9539828f19debaca4316fcf9a03f5273140
Juho Siitonen <juho_tuomas@msn.com>
2023-04-30 14:45:39 +0300

    Added new tests

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -6,10 +6,10 @@ level_map = ['00000000000000000000',
              '00000000000000000000',
              '00000000000000000000',
              'xxx000000x0000000000',
-             'xxx000000P0G00000xxx',
+             'xxx0000ACP0G00000xxx',
              '0000000xxxxx00000xxx',
              '0000000xxxxx00000xxx',
-             'xxxxx00000000B00E00B',
+             'xxxxx0000000000BE00B',
              'xxxxxxxx000000xxxxxx']
 
 CELL_SIZE = 64
@@ -49,7 +49,14 @@ class TestLevel(unittest.TestCase):
         player.direction.x = 1
         self.level.horizontal_collision()
         self.assertNotEqual(player.rect.x, 1100)
-    
+
+    def test_horizontal_collision_right_without_direction(self):
+        player = self.level.player_cell.sprite
+        player.rect.x = 1100
+        player.direction.x = 0
+        self.level.horizontal_collision()
+        self.assertEqual(player.rect.x, 1100)
+
     def test_vertical_collision_bottom(self):
         player = self.level.player_cell.sprite
         start = player.rect.y
@@ -66,6 +73,14 @@ class TestLevel(unittest.TestCase):
         self.level.vertical_collision()
         self.assertEqual(player.rect.y, start)
 
+    def test_vertical_collision_top_without_direction(self):
+        player = self.level.player_cell.sprite
+        start = player.rect.y
+        player.rect.y -= 40
+        player.direction.y = 0
+        self.level.vertical_collision()
+        self.assertEqual(player.rect.y, start-40)
+
     def test_level_completed(self):
         player = self.level.player_cell.sprite
         player.rect.x += 100
@@ -80,8 +95,12 @@ class TestLevel(unittest.TestCase):
         for enemy in self.level.enemies:
             start = enemy.rect.x
         self.level.enemy_movement()
+        self.level.enemy_movement()
+        self.level.enemy_movement()
+        self.level.enemy_movement()
         for enemy in self.level.enemies:
             self.assertNotEqual(enemy.rect.x, start)
+            self.assertEqual(enemy.direction, 1)
 
     def test_player_demise_out_of_bounds(self):
         player = self.level.player_cell.sprite
@@ -97,3 +116,22 @@ class TestLevel(unittest.TestCase):
     def test_player_demise_no_collision_in_bounds_false(self):
         self.assertEqual(self.level.player_demise(), False)
 
+    def test_coin_collection_success(self):
+        player = self.level.player_cell.sprite
+        player.rect.x -= 64
+        self.level.coin_collision()
+        self.assertEqual(self.level.player.coins, 1)
+
+    def test_no_coins_collected(self):
+        self.level.coin_collision()
+        self.assertEqual(self.level.player.coins, 0)
+
+    def test_artifact_collection_success(self):
+        player = self.level.player_cell.sprite
+        player.rect.x -= 128
+        self.level.artifact_collision()
+        self.assertEqual(self.level.player.artifacts, 1)
+
+    def test_no_artifacts_collected(self):
+        self.level.artifact_collision()
+        self.assertEqual(self.level.player.artifacts, 0)
