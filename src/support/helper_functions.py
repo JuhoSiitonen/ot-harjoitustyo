65977373a71317b0d42c8cee728ddb3f08be090e
Juho Siitonen <juho_tuomas@msn.com>
2023-05-05 12:45:09 +0300

    Added helper_functions module to read and write files

---
+++ src/support/helper_functions.py
@@ -0,0 +1,11 @@
+def level_file_reader():
+    all_lines, levels, single_level = [], [], []
+    with open("src/data/levels.txt") as f:
+        [all_lines.append(line.strip("\n")) for line in f.readlines()]
+    for line in all_lines:
+        if line == "-":
+            levels.append(single_level)
+            single_level = []
+        else:
+            single_level.append(line)
+    return levels
