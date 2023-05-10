57ffa77c52b3e5d0a446806c4a58379cd09f23d6
Juho Siitonen <juho_tuomas@msn.com>
2023-05-11 01:24:22 +0300

    Pylint corrections

--- src/build.py
+++ src/build.py
@@ -3,7 +3,7 @@ from database_initialization import initialize_db
 def build():
     """Function to call initializing of database, used with poetry invoke task.
     """
-    
+
     initialize_db()
 
 if __name__ == "__main__":
