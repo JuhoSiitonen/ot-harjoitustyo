dc5bd163a4d62feafae7af11afd2859997766611
Juho Siitonen <juho_tuomas@msn.com>
2023-05-10 23:53:41 +0300

    Added support function to read levels from file

--- src/support/helper_functions.py
+++ src/support/helper_functions.py
@@ -10,18 +10,3 @@ def level_file_reader():
         else:
             single_level.append(line)
     return levels
-
-def highscore_list_reader():
-    all_lines = []
-    with open("src/data/highscores.txt") as f:
-        [all_lines.append(line.strip("\n")) for line in f.readlines()]
-    return all_lines
-
-def highscore_list_checker(level_number, counter):
-    list = highscore_list_reader()
-    if not list:
-        pass
-    
-def highscore_list_writer(list):
-    with open("src/data/highscores.txt", "w") as f:
-        f.writelines(list)
